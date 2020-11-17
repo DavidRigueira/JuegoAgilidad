ab545b0121bc439547989007fccbd1f2f395e93f
DavidRigueira <david-rigi@hotmail.com>
2020-11-17 10:25:48 +0100

    anhado funcines

--- script.js
+++ script.js
@@ -17,22 +17,39 @@ function generarCirculo(){
     let y = numeroAleatorio(0, height)
     let radio = 100
     let nuevoCirculo = new Circulo(x,y,radio,color)
-    listaCirculos.push(nuevoCirculo)
-    colisionMouseCirculo(x, y, nuevoCirculo)
+    return nuevoCirculo
+    
 }
 function numeroAleatorio(min, max){
     return Math.round(Math.random() * (max-min) + min)
 }
 function setup(){
     createCanvas(400, 400)
+    let nuevoCirculo = generarCirculo()
+    listaCirculos.push(nuevoCirculo)
 }
 function draw(){
     background(220)
-    generarCirculo()
-    listaCirculos.forEach(circulo => circulo.dibujar())
+    listaCirculos.forEach(circulo => {
+        circulo.dibujar()
+        colisionMouseCirculo(circulo.x, circulo.y, circulo)
+    })
 }
 function colisionMouseCirculo(mx, my, circulo){
-    line(mx, 0, mx, 0)
-    line(0, my, 0, my)
-
+    let raton = line(mx, 0, mx, 0)
+    circulo = line(0, my, 0, my)
+    let colision = Math.sqrt(Math.pow(raton, 2) + Math.pow(circulo, 2))
+    if(colision > 0){
+        return false
+    }else{
+        return true
+    }
+}
+function mousePressed(){
+    let value = 0
+    if(value === 0){
+        value = 255
+    }else{
+        value = 0
+    }
 }
